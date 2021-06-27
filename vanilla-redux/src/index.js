4ebd00fc9dfd0c5e4f157f4f20057c2438dcfe17
YoujinCha <67622600+youjinDev@users.noreply.github.com>
2021-06-27 15:03:31 +0900

    vanilla JS setting

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,5 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const counter = document.querySelector(".counter");
+
+let number = 0;
+counter.innerHTML = number;
+
+const handlePlus = () => (counter.innerHTML = ++number);
+const handleMinus = () => (counter.innerHTML = --number);
+
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);
